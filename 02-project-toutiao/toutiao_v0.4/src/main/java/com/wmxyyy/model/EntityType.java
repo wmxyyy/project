4830eef4b9f9f49ee454dc6a5eb8f245f8a03d74
wmxyyy <wmxyyy>
2020-02-12 14:45:16 +0800

    project-toutiao v0.4

---
+++ 02-project-toutiao/toutiao_v0.4/src/main/java/com/wmxyyy/model/EntityType.java
@@ -0,0 +1,5 @@
+package com.wmxyyy.model;
+
+public class EntityType {
+    public static int ENTITY_NEWS = 1;
+}
---
+++ 02-project-toutiao/toutiao_v0.4/src/main/java/com/wmxyyy/service/NewsService.java
@@ -0,0 +1,58 @@
+package com.wmxyyy.service;
+
+import com.wmxyyy.dao.NewsDAO;
+import com.wmxyyy.model.News;
+import com.wmxyyy.util.ToutiaoUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class NewsService {
+    @Autowired
+    private NewsDAO newsDAO;
+
+    public List<News> getLatestNews(int userId, int offset, int limit) {
+        return newsDAO.selectByUserIdAndOffset(userId, offset, limit);
+    }
+
+    public int addNews(News news) {
+        newsDAO.addNews(news);
+        return news.getId();
+    }
+
+    public News getById(int newsId) {
+        return newsDAO.getById(newsId);
+    }
+
+    //图片上传
+    public String saveImage(MultipartFile file) throws IOException {
+        //判断是否有后缀
+        int dotPos = file.getOriginalFilename().lastIndexOf(".");
+        if (dotPos < 0) {
+            return null;
+        }
+        //判断后缀是否正确
+        String fileExt = file.getOriginalFilename().substring(dotPos + 1).toLowerCase();
+        if (!ToutiaoUtil.isFileAllowed(fileExt)) {
+            return null;
+        }
+        //设置图片名字
+        String fileName = UUID.randomUUID().toString().replaceAll("-", "") + "." + fileExt;
+        Files.copy(file.getInputStream(), new File(ToutiaoUtil.IMAGE_DIR + fileName).toPath(),
+                StandardCopyOption.REPLACE_EXISTING);
+        //返回图片地址
+        return ToutiaoUtil.TOUTIAO_DOMAIN + "image?name=" + fileName;
+    }
+
+    public int updateCommentCount(int id, int count) {
+        return newsDAO.updateCommentCount(id, count);
+    }
+}
